c0cd7329080eb572a4f274e875b8ece53eb99bd2
Adithya Mahesh Bariki <adithyamaheshbariki@gmail.com>
2017-06-26 22:58:15 -0500

    Added new files

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 var express = require('express');
 
 var app = express();
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
 app.get('/', function(req, res) {
-  res.send('hello World');
+    res.send('hello World');
 });
 
 app.get('/books', function(req, res) {
-  res.send('hello books');
+    res.send('hello books');
 });
 
 app.listen(port, function(err) {
-  console.log('running server on port' + port);
+    console.log('running server on port' + port);
 });
