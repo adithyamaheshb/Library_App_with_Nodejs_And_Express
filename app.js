bb4af9139215e6d6ab1ef6c9989f63e76e253bd0
Adithya Mahesh Bariki <adithyamaheshbariki@gmail.com>
2017-07-14 15:51:03 -0500

    updating some files

--- app.js
+++ app.js
@@ -2,12 +2,31 @@ var express = require('express');
 
 var app = express();
 var port = process.env.PORT || 5000;
-
+var nav = [{
+    Link: '/Books',
+    Text: 'Book'
+}, {
+    Link: '/Authors',
+    Text: 'Author'
+}];
+var bookRouter = require('./src/routes/bookRoutes')(nav);
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views', './src/views');
 
+app.set('view engine', 'ejs');
+
+app.use('/Books', bookRouter);
 app.get('/', function(req, res) {
-    res.send('hello World');
+    res.render('index', {
+        title: 'hello from render',
+        nav: [{
+            Link: '/Books',
+            Text: 'Books'
+        }, {
+            Link: '/Authors',
+            Text: 'Authors'
+        }]
+    });
 });
 
 app.get('/books', function(req, res) {
