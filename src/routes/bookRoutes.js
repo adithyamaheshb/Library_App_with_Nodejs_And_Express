662d896559a071f0d2261c69778f4621c8346c7b
Adithya Mahesh Bariki <adithyamaheshbariki@gmail.com>
2017-07-14 15:51:22 -0500

    updating some files

---
+++ src/routes/bookRoutes.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var bookRouter = express.Router();
+var router = function(nav) {
+    var books = [{
+            title: 'War and Peace',
+            genre: 'Historical Fiction',
+            author: 'Lev Nikolayevich Tolstoy',
+            read: false
+        },
+        {
+            title: 'Introduction to C Programming',
+            genre: 'Education',
+            author: 'Yashwanth Kanethkar',
+            read: false
+        },
+        {
+            title: 'CyberWar and Cyberterrorism',
+            genre: 'Education',
+            author: 'P.W.Singer',
+            read: false
+        }
+    ];
+
+    bookRouter.route('/')
+        .get(function(req, res) {
+            res.render('bookListView', {
+                title: 'Books',
+                nav: nav,
+                books: books
+            });
+        });
+
+    bookRouter.route('/:id')
+        .get(function(req, res) {
+            var id = req.params.id;
+            res.render('bookView', {
+                title: 'Books',
+                nav: nav,
+                book: books[id]
+            });
+        });
+    return bookRouter;
+};
+
+module.exports = router;
